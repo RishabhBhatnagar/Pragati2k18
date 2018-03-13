8e837899a2791a522822dd2a17633796eb4174bf
meandmyprogram <bhatnagarrishabh4@gmail.com>
2018-03-13 15:21:02 +0530

    onclick handled of bottom sheet

---
+++ app/src/main/java/pragatiblockchain/com/pragati2018/MainActivity.java
@@ -0,0 +1,94 @@
+package pragatiblockchain.com.pragati2018;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.BottomSheetDialog;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import pragatiblockchain.com.pragati2018.Fragments.ArtworkFragment;
+import pragatiblockchain.com.pragati2018.Fragments.ProfileFragment;
+import pragatiblockchain.com.pragati2018.Fragments.SearchFragment;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    private BottomSheetDialog bottomSheetDialog;
+    private View bottomSheetView;
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
+
+        loadFragment(new ArtworkFragment());
+
+
+
+
+
+        button = findViewById(R.id.button);
+        bottomSheetDialog = new BottomSheetDialog(this);
+        View bottomSheetView = getLayoutInflater().inflate(R.layout.bottom_sheet, null);
+        bottomSheetDialog.setContentView(bottomSheetView);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bottomSheetDialog.show();
+            }
+        });
+
+
+
+
+
+
+    }
+
+
+
+    private boolean loadFragment(Fragment fragment){
+        if(fragment!=null){
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        Fragment fragment = null;
+        switch(item.getItemId()){
+            case R.id.navigation_artwork:
+                fragment = new ArtworkFragment();
+                break;
+            case R.id.navigation_profile:
+                fragment = new ProfileFragment();
+                break;
+            case R.id.navigation_search:
+                fragment = new SearchFragment();
+                break;
+        }
+        return loadFragment(fragment);
+    }
+
+
+    public void imageView1(View view){
+        Toast.makeText(this, "imageView1", Toast.LENGTH_SHORT).show();
+    }
+    public void imageView2(View view){
+        Toast.makeText(this, "imageView2", Toast.LENGTH_SHORT).show();
+    }
+}
