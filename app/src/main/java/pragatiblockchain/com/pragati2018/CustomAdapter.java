6ba7559b120faec29e259ae0ffcb19c62327ff56
meandmyprogram <bhatnagarrishabh4@gmail.com>
2018-03-13 15:47:39 +0530

    added click listener for dynamically added cardlayout

--- app/src/main/java/pragatiblockchain/com/pragati2018/CustomAdapter.java
+++ app/src/main/java/pragatiblockchain/com/pragati2018/CustomAdapter.java
@@ -18,9 +18,11 @@ import java.util.List;
  */
 
 public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.CustomViewHolder> {
+
     private List<Arts> objectList;
     private LayoutInflater layoutInflater;
     private Context context;
+
     public CustomAdapter(Context context, List<Arts> objectList){
         layoutInflater = LayoutInflater.from(context);
         this.objectList = objectList;
@@ -35,8 +37,45 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.CustomView
 
     @Override
     public void onBindViewHolder(CustomViewHolder holder, final int position) {
+
+
+        holder.arts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context, "arts clicked"+String.valueOf(position), Toast.LENGTH_SHORT).show();
+            }
+        });
+        holder.donate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context, "donate clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+        holder.share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context, "share clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+        holder.like.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context, "like clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+        holder.info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context, "info clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+
         Arts current = objectList.get(position);
         holder.setData(current, position);
+
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -54,13 +93,22 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.CustomView
 
         private TextView title;
         private ImageView arts;
+        private ImageView donate;
+        private ImageView share;
+        private ImageView like;
+        private ImageView info;
+
         private int position;
         private Arts currentObject;
-        
+
         public CustomViewHolder(View itemView) {
             super(itemView);
-            title = itemView.findViewById(R.id.title);
-            arts = itemView.findViewById(R.id.art);
+            this.title = itemView.findViewById(R.id.title);
+            this.arts = itemView.findViewById(R.id.art);
+            this.donate = itemView.findViewById(R.id.donate);
+            this.share = itemView.findViewById(R.id.share);
+            this.like = itemView.findViewById(R.id.like);
+            this.info = itemView.findViewById(R.id.info);
         }
 
         public void setData(Arts currentObject, int position) {
